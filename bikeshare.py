1484e8eb9e628c99bdb17f96c6c8c881f34bfcfd
Marion <marion.bru@me.com>
2020-10-08 22:20:01 +0200

    comment for load_data function

--- bikeshare.py
+++ bikeshare.py
@@ -57,6 +57,7 @@ def get_filters():
 def load_data(city, month, day):
     """
     Loads data for the specified city and filters by month and day if applicable.
+    The data is loaded from an excel file 
 
     Args:
         (str) city - name of the city to analyze
